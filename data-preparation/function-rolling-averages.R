2e557eba93a38ce76bea894555eb0d5b7b9d122d
Kenji Ball <patrick.kenji.ball@gmail.com>
2018-05-04 17:28:43 +1000

    initial testing for rollapply to create rolling averages

--- data-preparation/function-rolling-averages.R
+++ data-preparation/function-rolling-averages.R
@@ -1,28 +1,19 @@
-
 # Test build some rolling average attributes for season
 
-# Account withdrawal features
-cat("calculating withdrawal profile features...\n")
+# Load required packages
+library(zoo)
 
-acc_trans_withdrawals_7days <- acc_trans_withdrawals[transaction_date >= observation_date - last_7_days]
-acc_trans_withdrawals_14days <- acc_trans_withdrawals[transaction_date >= observation_date - last_14_days]
-acc_trans_withdrawals_28days <- acc_trans_withdrawals[transaction_date >= observation_date - last_28_days]
-acc_trans_withdrawals_3months <- acc_trans_withdrawals[transaction_date >= observation_date - last_3_months]
-acc_trans_withdrawals_6months <- acc_trans_withdrawals[transaction_date >= observation_date - last_6_months]
-acc_trans_withdrawals_12months <- acc_trans_withdrawals[transaction_date >= observation_date - last_12_months]
+# Must run function-season-data first to have season data matrix available or must be passed.
 
-datasets <- list(acc_trans_withdrawals
-                 , acc_trans_withdrawals_7days
-                 , acc_trans_withdrawals_14days
-                 , acc_trans_withdrawals_28days
-                 , acc_trans_withdrawals_3months
-                 , acc_trans_withdrawals_6months
-                 , acc_trans_withdrawals_12months)
-periods <- c("", "_last_7_days", "_last_14_days", "_last_28_days",
-             "_last_3_months", "_last_6_months", "_last_12_months")
-results <- foreach(i=1:length(periods)) %dopar% {
-  result <- aggregate_withdrawal_features(datasets[[i]])
-  # make names like withdrawal_last_7_days ...
-  colnames(result)[-1] <- paste0(colnames(result)[-1], periods[[i]])
-  result
-}
+for_name_test <- "Penrith"
+
+season_2018_team <- subset(season_2018_datamatrix, for_name == for_name_test )
+
+
+
+rollapply(season_2018_team[7:10],3,mean, by.column = TRUE, fill = NA)
+
+sapply(season_2018_team,class)
+
+
+

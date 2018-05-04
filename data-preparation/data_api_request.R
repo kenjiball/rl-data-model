ad9584ca831f5293dc0ef335c79d11300e4609d7
kenjiball <37519901+kenjiball@users.noreply.github.com>
2018-05-04 15:46:23 +1000

    Add files via upload

---
+++ data-preparation/data_api_request.R
@@ -0,0 +1,43 @@
+# This is  first version attempt to scrape NRL data from Fox Sports
+# Using the fox sports stats API and Key
+
+# Import local variables from file
+
+getwd()
+setwd(wd)
+
+
+# Call packages
+library(curl)
+library(jsonlite)
+
+
+# Function to Read Player Data Json from API and save to file 
+grabData <- function(gameid,api_url,api_key,fileprefix){
+  # Create API call from gameid
+  API_str <- paste(api_url
+               ,gameid
+               ,api_key
+               , sep = "", collapse = NULL)
+  # Create fie name from gameid
+  file_name <- paste(fileprefix,gameid,".json", sep = "", collapse = NULL)
+  # Call API to get data for a game
+  jsonData <- fromJSON(API_str)
+  jsonDataPretty <- toJSON(jsonData, pretty = TRUE)
+  # Write to file
+  write(jsonDataPretty, file_name)
+}
+
+
+# Single game 
+grabData(20180101,api_url,api_player_key,"playerdata_")
+grabData(20180101,api_url,api_match_key,"matchdata_")
+grabData(20180101,api_url,api_scoring_key,"scoringdata_")
+
+
+# Vector of games
+                 
+sapply(season_2018, grabData,api_url,api_player_key,"playerdata_")
+sapply(season_2018, grabData,api_url,api_match_key,"matchdata_")
+sapply(season_2018, grabData,api_url,api_scoring_key,"scoringdata_")
+

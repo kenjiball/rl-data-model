32df31c33e153863a2921e9e9c69cfc239b291b3
kball-tabcorp <kenji.ball@tabcorp.com.au>
2018-05-14 14:46:16 +1000

    Updated to include Neural Network model

--- data-preparation/logit-regression-model.R
+++ data-preparation/logit-regression-model.R
@@ -8,6 +8,7 @@
 library(Amelia)
 library(rminer)
 library(pscl)
+library(neuralnet)
 
 # define the data set to be used
 nrl_data <- season_2018_datamatrix
@@ -16,14 +17,24 @@ nrl_data <- season_2018_datamatrix
 missmap(nrl_data, main = "Missing values vs observed")
 
 # Define a training set and the hold out set
-nrl_holdout <- holdout(nrl_data$match_id, ratio = 4/5, internalsplit = FALSE, mode = "random",  seed = 1)
+# holdout set MUST be ORDERED and by Round
+# nrl_holdout <- holdout(nrl_data$match_id, ratio = 4/5, internalsplit = FALSE, mode = "random",  seed = 2)
+
+round_factors <- levels(as.factor(nrl_data$round))
+ratio <- 3/4
+nrl_train_ind <- floor(length(round_factors)*ratio)
+nrl_train <- subset(nrl_data, as.numeric(round) <= nrl_train_ind )
+nrl_test <- subset(nrl_data, as.numeric(round) > nrl_train_ind )
+
+levels(as.factor(nrl_train$round))
+levels(as.factor(nrl_test$round))
+dim(nrl_train)
+dim(nrl_test)
 
 # nrl_holdout$tr is the training set indexes
-length(nrl_holdout$tr)
-nrl_tr <- nrl_data[nrl_holdout$tr,]
+# nrl_tr <- nrl_data[nrl_holdout$tr,]
 # nrl_holdout$tr is the model test set indexes
-length(nrl_holdout$ts)
-nrl_ts <- nrl_data[nrl_holdout$ts,]
+# nrl_ts <- nrl_data[nrl_holdout$ts,]
 
 # Run the glm Model 
 # Initally using the binomial logit fit for the match result
@@ -50,12 +61,17 @@ choose.columns <- c("for_match_result",
                     "for_run_metres",
                     "for_tackle_busts_per_run")
 
-nrl_tr_trim <- nrl_tr[choose.columns]
-nrl_ts_trim <- nrl_ts[choose.columns]
+nrl_train_trim <- nrl_train[choose.columns]
+nrl_test_trim <- nrl_test[choose.columns]
 
-contrasts(nrl_tr_trim$for_match_result)
+dim(nrl_train_trim)
+dim(nrl_test_trim)
 
-model <- glm(for_match_result ~.,family=binomial(link='logit'),data=nrl_tr_trim)
+contrasts(nrl_train_trim$for_match_result)
+
+# Run Logit model
+
+model <- glm(for_match_result ~.,family=binomial(link='logit'),data=nrl_train_trim)
 
 # See summary of model to determine Deterministic (significant) Variables
 # Remember that in the logit model the response variable is log odds: 
@@ -69,16 +85,71 @@ pR2(model)
 
 # Test the accuracy of the model using the test hold out cell
 
-fitted.results <- predict(model,nrl_ts_trim,type='response')
+fitted.results <- predict(model,nrl_test_trim,type='response')
 fitted.results2 <- ifelse(fitted.results > 0.5,"Lose","Win")
-misClasificError <- mean(fitted.results2 != nrl_ts_trim$for_match_result)
+misClasificError <- mean(fitted.results2 != nrl_test_trim$for_match_result)
 print(paste('Accuracy',1-misClasificError))
 
-see_results <- data.frame(nrl_ts$for_name, nrl_ts$against_name,
-                          nrl_ts$for_points, nrl_ts$against_points,
-                          fitted.results, fitted.results2, nrl_ts_trim)
+see_results <- data.frame(nrl_test$for_name, nrl_test$against_name,
+                          nrl_test$for_points, nrl_test$against_points,
+                          fitted.results, fitted.results2, nrl_test_trim)
 
 write.csv(see_results,file="../model_test_results.csv")
+
+# Run Neural Network Model
+
+# Replace Win/Loss in for_match_result
+nrl_train_trim_s <- nrl_train_trim
+nrl_train_trim_s$for_match_result <- sapply(nrl_train_trim_s$for_match_result,function(x) {x <- gsub("Win",1,x)})
+nrl_train_trim_s$for_match_result <- sapply(nrl_train_trim_s$for_match_result,function(x) {x <- gsub("Lose",0,x)})
+nrl_train_trim_s$for_match_result <- as.numeric(nrl_train_trim_s$for_match_result)
+
+nrl_test_trim_s <- nrl_test_trim
+nrl_test_trim_s$for_match_result <- sapply(nrl_test_trim_s$for_match_result,function(x) {x <- gsub("Win",1,x)})
+nrl_test_trim_s$for_match_result <- sapply(nrl_test_trim_s$for_match_result,function(x) {x <- gsub("Lose",0,x)})
+nrl_test_trim_s$for_match_result <- as.numeric(nrl_test_trim_s$for_match_result)
+
+
+# Normalise data so each variable is scaled from 0 to 1
+max_value <- as.numeric(sapply(nrl_train_trim_s, max))
+min_value <- as.numeric(sapply(nrl_train_trim_s, min))
+scale_value <- max_value - min_value
+nrl_train_trim_norm <- as.data.frame(scale(nrl_train_trim_s, center = min_value, scale = scale_value))
+
+max_value <- as.numeric(sapply(nrl_test_trim_s, max))
+min_value <- as.numeric(sapply(nrl_test_trim_s, min))
+scale_value <- max_value - min_value
+nrl_test_trim_norm <- as.data.frame(scale(nrl_test_trim_s, center = min_value, scale = scale_value))
+
+
+head(nrl_train_trim_norm)
+
+# Set up the formula to define the prediction and the input variables.
+
+all_vars <- colnames(nrl_train_trim_norm)
+predictor_var <- "for_match_result"
+input_vars <- paste(all_vars[!all_vars%in%predictor_var], collapse = "+")
+formula_vars <-  as.formula(paste(paste(predictor_var,"~"),input_vars, collapse = "+"))
+
+
+# Create Neural Network Model
+neural_model <- neuralnet(formula = formula_vars, hidden = 4, linear.output = T, data = nrl_train_trim_norm)
+
+plot(neural_model)
+
+# Predit result using neural network and test data
+predict_games <- compute(neural_model, nrl_test_trim_norm[,2:20])
+
+# Measure the accuracy of the Predictions
+predict_games_result <- predict_games$net.result*(max(nrl_test_trim_norm$for_match_result)-min(nrl_test_trim_norm$for_match_result))+min(nrl_test_trim_norm$for_match_result)
+actual_games_result <- nrl_test_trim_norm$for_match_result * (max(nrl_test_trim_norm$for_match_result)-min(nrl_test_trim_norm$for_match_result))+min(nrl_test_trim_norm$for_match_result)
+
+MSE <- sum((predict_games_result - actual_games_result)^2)/nrow(nrl_test_trim_norm)
+MSE
+
+plot_data <- data.frame(as.data.frame(predict_games_result),as.data.frame(actual_games_result))
+names(plot_data) <- c("prediction","actual")
+plot(plot_data$prediction, plot_data$actual)
 
 
 dim(nrl_data)

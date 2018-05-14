654bd480383ed90c5f28e5710b7ef28d903774a3
kball-tabcorp <kenji.ball@tabcorp.com.au>
2018-05-14 16:42:12 +1000

    updates to roll_mean_data to improve functionality.

--- data-preparation/function-rolling-averages.R
+++ data-preparation/function-rolling-averages.R
@@ -5,55 +5,57 @@ library(zoo)
 
 # Must run function-season-data first to have season data matrix available or must be passed.
 
-roll_mean_data <- function(data, team, window){
-
-    # team <- "Penrith"
-    # columns <- "[7:80,82:155]" 
-    # window <- 5
+roll_mean_data <- function(data, team, window, app_name,header){
+      
+    #data <- season_2018_datamatrix
+    #team <- "Penrith"
+    #window <- 5
   
-    season_2018_team <- subset(data, for_name == for_name_test )
+    data <- subset(data, for_name == for_name_test )
     
-    header_team <- season_2018_team[c(1:6,81)]
-    data_roll <- season_2018_team[c(7:80,82:155)] # season_2018_team[c(7:80,82:155)]
+    header_team <- data[,which(!sapply(data,is.numeric))]
+    data_roll <- data[,which(sapply(data,is.numeric))] # season_2018_team[c(7:80,82:155)]
     
-    rollmean_games <- rollapply(data_roll,window,mean, by.column = TRUE, partial = TRUE, align = "right")
+    rollmean_games <- rollapply(data_roll,window,mean, by.column = TRUE, partial = TRUE, align = "right", fill = 0)
     
-    colnames(rollmean_games) <- paste(colnames(rollmean_games),"last3", sep = "_")
+    colnames(rollmean_games) <- paste(colnames(rollmean_games),app_name, sep = "_")
     
-    cbind(header_team,rollmean_games)
+    if(header == TRUE){
+      result <- data.frame(header_team,rollmean_games)
+    }else{
+      result <-  as.data.frame(rollmean_games)
+    }
     
-    return(rollmean_games)
+    return(result)
 
 }
 
-roll_mean_data(season_2018_datamatrix,"Penrith",3)
+roll1_data <- roll_mean_data(season_2018_datamatrix,"Penrith",list(-1:-1),"last1",TRUE)
+roll3_data <- roll_mean_data(season_2018_datamatrix,"Penrith",list(-1:-3),"last3",FALSE)
+roll5_data <- roll_mean_data(season_2018_datamatrix,"Penrith",list(-1:-5),"last5",FALSE)
+rollseason_data <- roll_mean_data(season_2018_datamatrix,"Penrith",list(-1:-30),"seasonavg",FALSE)
+
+panthers <-subset(data, for_name == "Penrith" )
+team_rollingave_df <- data.frame(panthers,roll1_data, roll3_data, roll5_data, rollseason_data)
+
+dim(team_rollingave_df)
+
+write.csv(team_rollingave_df, "../panthers_avg.csv")
+
+# Need to create an extra blank row for the upcming match with the prev avgs but no match outcome
 
 
+# testing below
+panthers <-subset(data, for_name == "Penrith" )
+write.csv(team_rollingave_df, "../panthers_avg.csv")
+
+rollapply(panthers$for_errors,list(-1:-3),mean, by.column = TRUE, partial = TRUE, align = "right", fill = 0)
+season_2018_datamatrix[,numeric_list]
 class_list <- sapply(season_2018_team,class)
 
-# check if all columns in data_roll are numeric
-length(lapply(data_roll, is.numeric)) == sum(unlist(lapply(data_roll, is.numeric)))
 
 
 
-
-# playing with correlation
-
-
-cor_data <- season_2018_datamatrix[c(7:83,85:161)]
-
-cor_test <- cor(cor_data)
-write.csv(cor_test, "../correlation_test.csv")
-
-# Testing to see the structure of sets
-sets <- data.frame(season_2018_datamatrix$for_complete_sets, season_2018_datamatrix$for_inCompleteSets
-          , season_2018_datamatrix$for_total_sets, season_2018_datamatrix$for_penaltiesAwarded
-          , season_2018_datamatrix$for_errors, season_2018_datamatrix$for_tries
-          , season_2018_datamatrix$for_goal_attempted, season_2018_datamatrix$for_kicks)
-names(sets) <- c("for_complete_sets","for_inCompleteSets","for_total_sets","for_penaltiesAwarded"
-                 ,"for_errors","for_tries","for_goal_attempted","for_kicks")
-
-head(sets,10)
 
 
 

ec18b7bb1e99c66f7304af34f7ceaba6c5ebef73
kenjiball <patrick.kenji.ball@gmail.com>
2018-08-03 12:31:28 +1000

    New model to predict the number of tries using a poisson regression

---
+++ model/poisson-regression-model.R
@@ -0,0 +1,115 @@
+# First attempt at Logistic Regression using NRL season-data matrix
+# Example run taken from the following blog article:
+# https://datascienceplus.com/perform-logistic-regression-in-r/
+# https://gist.github.com/mick001/ac92e7c017aecff216fd
+
+# Load any required packages
+
+library(Amelia)
+library(rminer)
+library(pscl)
+library(neuralnet)
+library(caret)
+library(dplyr)
+library(pROC)
+
+
+# define the data set to be used
+nrl_data <- season_2017_datamatrix
+
+# Quick plot to observe if there is any missing data
+missmap(nrl_data, main = "Missing values vs observed")
+
+# Define a training set and the hold out set
+# holdout set MUST be ORDERED and by Round
+# nrl_holdout <- holdout(nrl_data$match_id, ratio = 4/5, internalsplit = FALSE, mode = "random",  seed = 2)
+
+round_factors <- levels(as.factor(nrl_data$round))
+ratio <- 3/4
+nrl_train_ind <- floor(length(round_factors)*ratio)
+nrl_train <- subset(nrl_data, as.numeric(round) <= nrl_train_ind )
+nrl_test <- subset(nrl_data, as.numeric(round) > nrl_train_ind )
+
+table(nrl_train$round)
+table(nrl_test$round)
+dim(nrl_train)
+dim(nrl_test)
+
+
+# Run the glm Model 
+# Initally using the binomial logit fit for the match result
+
+# model 1 training set
+
+nrl_train_trim_m1 <- nrl_train %>% 
+  filter(Home_Away == "Home")%>% 
+  select(for_tries
+         # Retained Features
+         , for_completion_rate, for_average_run_metres, for_tackle_busts_per_run, for_run_metres_success_rate
+
+  )
+
+
+
+# model 2 training set
+
+nrl_train_trim_m2 <- nrl_train %>% 
+                   filter(Home_Away == "Home")%>% 
+                   select(for_tries
+                          # Retained Features
+                          , for_completion_rate, for_average_run_metres, for_tackle_busts_per_run, for_run_metres_success_rate
+                          # Testing Features
+                          , for_penaltiesAwarded #, for_runs_8plus_meters, for_penaltiesAwarded, for_tackledOpp20
+                   )
+# set set holout data
+nrl_test_trim <-    nrl_test %>% 
+                    filter(Home_Away == "Home")%>% 
+                    select(for_tries)
+
+
+#all_equal(nrl_test_trim_m1,nrl_test_trim2, convert = TRUE, ignore_col_order = TRUE)
+
+contrasts(as.factor(nrl_train_trim$for_tries))
+
+# Run Logit model m1 and m2
+
+model_m1 <- glm(for_tries ~ . , data=nrl_train_trim_m1 , family=poisson())
+model_m2 <- glm(for_tries ~ . , data=nrl_train_trim_m2 , family=poisson())
+
+# See summary of model to determine Deterministic (significant) Variables
+# Remember that in the logit model the response variable is log odds: 
+# ln(odds) = ln(p/(1-p)) = a*x1 + b*x2 + … + z*xn
+# McFadden R2 index can be used to assess the model fit
+# McFadden's pseudo R-squared ranging from 0.2 to 0.4 indicates very good model fit
+
+summary(model_m1)
+summary(model_m2)
+anova(model_m2, model_m1, test="Chisq")
+pR2(model_m1)
+pR2(model_m2)
+
+# Test the accuracy of the model using the test hold out cell
+nrl_test_trim$predicted_tries_m1 <-  predict(model_m1,nrl_test_trim_m1,type='response')
+nrl_test_trim$predicted_tries_m2 <-  predict(model_m2,nrl_test_trim_m2,type='response')
+
+cor(nrl_test_trim$for_tries,nrl_test_trim$predicted_tries_m1)
+cor(nrl_test_trim$for_tries,nrl_test_trim$predicted_tries_m2)
+
+# chart/ print results and predictions 
+gather(nrl_test_trim, model, predicted_tries, -for_tries ) #####
+
+
+ggplot(nrl_test_trim, aes(x = as.factor(for_tries), y = predicted_tries_m1))+
+  geom_boxplot() +
+  geom_abline(slope=1, intercept = 1 ) +
+  expand_limits(x = 0, y = 0)
+
+
+# Build a ROC curve to graph model performance
+ROC <-roc(nrl_test_trim2$for_tries,nrl_test_trim2$predicted_tries)
+plot.new()
+plot(ROC, col="blue")
+auc(ROC)
+
+
+

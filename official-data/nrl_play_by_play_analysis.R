461f37639065db45ddf1e10f5a9cea0afb7302be
kenjiball <patrick.kenji.ball@gmail.com>
2019-04-15 17:28:49 +1000

    included new linear model for expect points and study for metres gained

--- official-data/nrl_play_by_play_analysis.R
+++ official-data/nrl_play_by_play_analysis.R
@@ -17,13 +17,18 @@ transitionDataset_2 <- transitionDataset %>%
     home_away       = substr(state, nchar(state), nchar(state)),
     home_away_prev  = lag(home_away),
     new_set_flag    = coalesce(if_else(home_away != home_away_prev | lag(toTransition) == "try", TRUE, FALSE),TRUE),
+    end_set_flag    = if_else(lead(new_set_flag) == TRUE, TRUE, FALSE),
     set_start_coord = if_else(new_set_flag == TRUE, xCoord, 0),
+    set_end_coord   = if_else(end_set_flag == TRUE, xCoord, 0),
     set_id          = cumsum(new_set_flag)
     
   ) %>% 
   group_by(set_id) %>% 
   mutate(
-    total_tries_per_set  = sum( toTransition == "try" , na.rm = TRUE)
+    total_tries_per_set  = sum( toTransition == "try" , na.rm = TRUE),
+    metres_gained        = abs(sum(set_end_coord, na.rm = TRUE) - sum(set_start_coord, na.rm = TRUE)),
+    tackles_per_set      = sum(toTransition == "tackled", na.rm = TRUE),
+    penalties_per_set    = sum(toTransition == "penalty", na.rm = TRUE)
   ) %>% 
   ungroup()
 
@@ -31,9 +36,29 @@ transitionDataset_2 <- transitionDataset %>%
 
 table(transitionDataset$toTransition)
 table(transitionDataset_2$total_tries_per_set)
+table(transitionDataset_2$tackles_per_set)
+table(transitionDataset_2$penalties_per_set)
+
+# metres gained distribution
+transitionDataset_2 %>% 
+  filter(penalties_per_set == 1 & metres_gained > 0) %>% 
+  select(set_id, metres_gained, total_tries_per_set, penalties_per_set) %>% 
+  distinct() %>% 
+  ggplot( aes(x = metres_gained) ) +
+  #facet_grid(total_tries_per_set~. , scales = "free" ) +
+  geom_histogram(binwidth = 5)
 
 
-# exp points
+transitionDataset_2 %>%
+  select(set_id, metres_gained, total_tries_per_set, penalties_per_set) %>% 
+  distinct() %>% 
+  group_by(total_tries_per_set, penalties_per_set) %>% 
+  summarise( count = n()
+           )
+
+
+
+# exp points modelling
 
 transitionDataset_3 <- transitionDataset_2 %>%
   filter(new_set_flag == TRUE ) %>% 
@@ -45,12 +70,40 @@ transitionDataset_3 <- transitionDataset_2 %>%
              mean_points  = mean_tries * 4 ) %>% 
   ungroup() %>% 
   mutate(
-    freq  = count / sum(count, na.rm = TRUE)
+    freq  = count / sum(count, na.rm = TRUE),
+    xCoord = as.numeric(xCoord_bands),
+    xCoord_mid = c(-5.5:5.5, NA)*10
   )
-  
+
+
+# ggplot
+# linear model with bands
 transitionDataset_3 %>% 
-  ggplot( aes( x = xCoord_bands, y = mean_tries ))+
-  geom_point()
+  filter(!is.na(xCoord_bands) & xCoord_bands != "(50,60]") %>% 
+  ggplot( aes( x = xCoord_mid, y = mean_tries )) +
+  geom_point() + 
+  geom_smooth(method = lm, se = FALSE)
+
+transitionDataset_4 <- transitionDataset_3 %>% 
+  filter(!is.na(xCoord_bands) & xCoord_bands != "(50,60]")
+
+linear_exp_tries_model <- lm(mean_tries~xCoord_mid, data = transitionDataset_4)
+summary(linear_exp_tries_model)
+
+# linear model
+transitionDataset_2 %>%
+  filter(new_set_flag == TRUE & set_start_coord >= -60 & set_start_coord <= 50 ) %>% 
+  select(set_id, set_start_coord, total_tries_per_set) %>% 
+  ggplot( aes( x = set_start_coord, y = total_tries_per_set )) +
+  geom_point() + 
+  geom_smooth(method = "lm", 
+              se = FALSE)
+
+
+
+
+
+
 
 
 

33b45d0481e9438058126dd03d7d8ec785d514d6
kenjiball <patrick.kenji.ball@gmail.com>
2019-07-15 13:08:18 +1000

    Major workflow overhaul to write to file and only updated required rounds by using new function write_load_nrl_match_data.

--- official-data/get_nrl_data.R
+++ official-data/get_nrl_data.R
@@ -20,100 +20,71 @@ nrl_round_id_df  <- get_nrl_id_info("round", draw_url)
 nrl_comp_id_df   <- get_nrl_id_info("comp", draw_url)
 nrl_season_id_df <- get_nrl_id_info("season", draw_url)
 
+# Save to file
+write_csv(nrl_team_id_df, paste0(wd_data, "/nrl_tables/", "nrl_team_id_2019.csv"))
+write_csv(nrl_round_id_df, paste0(wd_data, "/nrl_tables/", "nrl_round_id_2019.csv"))
+write_csv(nrl_comp_id_df, paste0(wd_data, "/nrl_tables/", "nrl_comp_id_2019.csv"))
+write_csv(nrl_season_id_df, paste0(wd_data, "/nrl_tables/", "nrl_season_id_2019.csv"))
+
 
 # Getting nrl draw as a list
-# draw_2019_team <- lapply(1:16 , function(i)get_nrl_draw(111,2019,i, "team"), draw_url)
-draw_2010_round <- lapply(1:30 , function(i)get_nrl_draw(111,2010,i, "round", draw_url))
-draw_2011_round <- lapply(1:30 , function(i)get_nrl_draw(111,2011,i, "round", draw_url))
-draw_2012_round <- lapply(1:30 , function(i)get_nrl_draw(111,2012,i, "round", draw_url))
-
 # match and player data only beginfs from 2013 season
-draw_2013_round <- lapply(1:30 , function(i)get_nrl_draw(111,2013,i, "round", draw_url))
-draw_2014_round <- lapply(1:30 , function(i)get_nrl_draw(111,2014,i, "round", draw_url))
-draw_2015_round <- lapply(1:30 , function(i)get_nrl_draw(111,2015,i, "round", draw_url))
-draw_2016_round <- lapply(1:30 , function(i)get_nrl_draw(111,2016,i, "round", draw_url))
-draw_2017_round <- lapply(1:30 , function(i)get_nrl_draw(111,2017,i, "round", draw_url))
 draw_2018_round <- lapply(1:29 , function(i)get_nrl_draw(111,2018,i, "round", draw_url))
-
 draw_2019_round <- lapply(1:25  , function(i)get_nrl_draw(111,2019,i, "round", draw_url))
 
+# write to file
+test <- toJSON( draw_2018_round, pretty = TRUE)
+write(test, paste0(wd_data, "/nrl_draw/", "nrl_draw_2018.json"))
+rm(test, draw_2018_round)
+
+
+#### Load URLs
+# 2013 - 2017: Rounds 1:30
+# 2018 - 2019: Rounds 1:29
+
 # set rounds
-rounds <- 1:12
-
-# get Match URLs to get match data
-match_url_lookup_2013 <- lapply( 1:30, function(i) unlist(draw_2013_round[[i]]$matchCentreUrl))
-match_url_lookup_2014 <- lapply( 1:30, function(i) unlist(draw_2014_round[[i]]$matchCentreUrl))
-match_url_lookup_2015 <- lapply( 1:30, function(i) unlist(draw_2015_round[[i]]$matchCentreUrl))
-match_url_lookup_2016 <- lapply( 1:30, function(i) unlist(draw_2016_round[[i]]$matchCentreUrl))
-match_url_lookup_2017 <- lapply( 1:30, function(i) unlist(draw_2017_round[[i]]$matchCentreUrl))
-match_url_lookup_2018 <- lapply( 1:29, function(i) unlist(draw_2018_round[[i]]$matchCentreUrl))
-
-match_url_lookup_2019 <- lapply( rounds , function(i) unlist(draw_2019_round[[i]]$matchCentreUrl))
-
-
-### get match data into list
-# create a list of urls to apply over
-match_urls_2013 <- unlist(match_url_lookup_2013[1:30])[!is.na(unlist(match_url_lookup_2013[1:30]))]
-match_urls_2014 <- unlist(match_url_lookup_2014[1:30])[!is.na(unlist(match_url_lookup_2014[1:30]))]
-match_urls_2015 <- unlist(match_url_lookup_2015[1:30])[!is.na(unlist(match_url_lookup_2015[1:30]))]
-match_urls_2016 <- unlist(match_url_lookup_2016[1:30])[!is.na(unlist(match_url_lookup_2016[1:30]))]
-match_urls_2017 <- unlist(match_url_lookup_2017[1:30])[!is.na(unlist(match_url_lookup_2017[1:30]))]
-match_urls_2018 <- unlist(match_url_lookup_2018[1:29])[!is.na(unlist(match_url_lookup_2018[1:29]))]
-
-match_urls_2019 <- unlist(match_url_lookup_2019[rounds])[!is.na(unlist(match_url_lookup_2019[rounds]))]
-
-# get nrl match data
-match_data_2013 <- lapply(match_urls_2013, function(i)get_nrl_match_data(i, web_url))
-match_data_2014 <- lapply(match_urls_2014, function(i)get_nrl_match_data(i, web_url))
-match_data_2015 <- lapply(match_urls_2015, function(i)get_nrl_match_data(i, web_url))
-match_data_2016 <- lapply(match_urls_2016, function(i)get_nrl_match_data(i, web_url))
-match_data_2017 <- lapply(match_urls_2017, function(i)get_nrl_match_data(i, web_url))
-match_data_2018 <- lapply(match_urls_2018, function(i)get_nrl_match_data(i, web_url))
-
-match_data_2019 <- lapply(match_urls_2019, function(i)get_nrl_match_data(i, web_url))
-
-#### Extract Match data
+rounds <- 1:17
 # define number of mataches for current season
 matches <- 1:length(match_urls_2019)
 
-match_table_2010_df <- list_df2df(lapply(1:201, function(i)extract_nrl_match_data(match_data_2010,i)))
+# Load draw files from json
+draw_2019_round <- fromJSON(paste0(wd_data, "/nrl_draw/", "nrl_draw_2019.json"))
+match_url_lookup_2019 <- unlist(lapply( rounds, function(i) unlist(draw_2019_round[[i]]$matchCentreUrl)))
+# Remove NAs
+match_urls_2019 <- match_url_lookup_2019[!is.na(match_url_lookup_2019)]
+# write to file
+write_csv(as.data.frame(match_urls_2019), paste0(wd_data, "/nrl_url/", "match_url_2019.csv"), col_names = FALSE)
 
-match_table_2013_df <- list_df2df(lapply(1:201, function(i)extract_nrl_match_data(match_data_2013,i)))
-match_table_2014_df <- list_df2df(lapply(1:201, function(i)extract_nrl_match_data(match_data_2014,i)))
-match_table_2015_df <- list_df2df(lapply(1:201, function(i)extract_nrl_match_data(match_data_2015,i)))
-match_table_2016_df <- list_df2df(lapply(1:201, function(i)extract_nrl_match_data(match_data_2016,i)))
-match_table_2017_df <- list_df2df(lapply(1:201, function(i)extract_nrl_match_data(match_data_2017,i)))
+##### New function to write or load data
+# OLD WAY: match_data_2019 <- sapply(match_urls_2019, function(i)get_nrl_match_data(i, web_url))
+# Typical season has 201 matches.
+match_data_2014 <- write_load_nrl_match_data(2014, 1:201, wd_data, load_full_year = TRUE, update_files = FALSE)
+match_data_2019 <- write_load_nrl_match_data(2019, 1:128, wd_data, load_full_year = TRUE, update_files = FALSE)
+
+
+# STEP 4: Data is extracted from the match data list.
+# Run apply to extract player and team data
 match_table_2018_df <- list_df2df(lapply(1:201, function(i)extract_nrl_match_data(match_data_2018,i)))
+player_2018_df <- bind_rows(lapply(1:201, function(i)extract_nrl_player_data(match_data_2018,i)))
 
-match_table_2019_df <- list_df2df(lapply(matches, function(i)extract_nrl_match_data(match_data_2019,i)))
+# Need to fix this in the extract_nrl_player_data function
+player_2016_df_new$stintTwo <- as.numeric(player_2016_df_new$stintTwo)
 
-# TO DO: Fix issue of factor to numeric conversion in the function
-match_table_2019_df$matchId <- as.numeric(as.character(match_table_2019_df$matchId))
-match_table_2019_df$startTime <- as.POSIXct(match_table_2019_df$startTime)
 
 match_table_df <- bind_rows(match_table_2013_df, match_table_2014_df, match_table_2015_df, match_table_2016_df,
                             match_table_2017_df, match_table_2018_df, match_table_2019_df)
-
-#### Extract Player data
-player_2013_df <- bind_rows(lapply(1:201, function(i)extract_nrl_player_data(match_data_2013,i)))
-player_2014_df <- bind_rows(lapply(1:201, function(i)extract_nrl_player_data(match_data_2014,i)))
-player_2015_df <- bind_rows(lapply(1:201, function(i)extract_nrl_player_data(match_data_2015,i)))
-player_2016_df <- bind_rows(lapply(1:201, function(i)extract_nrl_player_data(match_data_2016,i)))
-player_2017_df <- bind_rows(lapply(1:201, function(i)extract_nrl_player_data(match_data_2017,i)))
-player_2018_df <- bind_rows(lapply(1:201, function(i)extract_nrl_player_data(match_data_2018,i)))
-
-player_2019_df <- bind_rows(lapply(matches, function(i)extract_nrl_player_data(match_data_2019,i)))
-
-# TO DO: Fix issue of factor to numeric conversion in the function
-player_2019_df$matchId <- as.numeric(player_2019_df$matchId)
 
 player_df <- bind_rows(player_2013_df, player_2014_df, player_2015_df, player_2016_df,
                             player_2017_df, player_2018_df, player_2019_df)
 
 
-# Extract current season data and upload to drive
+
+player_df %>% 
+  group_by(season, roundNumber) %>% 
+  summarise( n = n()/34)
 
 
+### Extract current season data and upload to drive
 # Load data to Googledrive
 # use googledrive package to upload file to drive
 upload_to_drive(match_table_df, gdrive_path_id)
@@ -136,57 +107,8 @@ match_data_2019[[22]]$homeTeam$coaches
 
 match_data_2019[[22]]$homeTeam$captainPlayerId
 
+# discipline filed for sin bins
 
-
-
-
-### play the ball speed by team and position
-mu <- player_df %>%
-  filter(playTheBallAverageSpeed > 0 & playTheBallAverageSpeed < 6) %>% 
-  mutate( total_playTheBall_time = playTheBallTotal*playTheBallAverageSpeed) %>% 
-  group_by(name, position) %>% 
-  summarise( grp.mean=mean(playTheBallAverageSpeed),
-             total.mean = sum(total_playTheBall_time)/sum(playTheBallTotal)  )
-
-player_df %>%
-  filter(playTheBallAverageSpeed > 0 & playTheBallAverageSpeed < 6) %>% 
-  ggplot(aes(playTheBallAverageSpeed , color = name)) +
-  geom_density() +
-  geom_vline(data=mu, aes(xintercept=total.mean, color=name), linetype="dashed") +
-  facet_grid(position~., scales = "free") + 
-  scale_color_manual(values=c(rainbow(16)))
-  
-### play the ball speed by team and season
-mu <- match_table_df_2 %>% 
-  inner_join(player_df, by = "matchId") %>%
-  filter(playTheBallAverageSpeed > 0 & playTheBallAverageSpeed < 6) %>% 
-  mutate( total_playTheBall_time = playTheBallTotal*playTheBallAverageSpeed,
-          oppositionTeam = if_else(name == homeTeam, awayTeam, homeTeam),
-          oppositionTeam_is_souths  = if_else(oppositionTeam == 'South Sydney Rabbitohs', TRUE, FALSE)
-          ) %>% 
-  group_by(oppositionTeam_is_souths, year) %>% 
-  summarise( grp.mean=mean(playTheBallAverageSpeed),
-             total.mean = sum(total_playTheBall_time)/sum(playTheBallTotal)  )
-
-match_table_df_2 %>% 
-  inner_join(player_df, by = "matchId") %>%
-  filter(playTheBallAverageSpeed > 0 & playTheBallAverageSpeed < 6) %>% 
-  mutate( total_playTheBall_time = playTheBallTotal*playTheBallAverageSpeed,
-          oppositionTeam = if_else(name == homeTeam, awayTeam, homeTeam),
-          oppositionTeam_is_souths  = if_else(oppositionTeam == 'South Sydney Rabbitohs', TRUE, FALSE)
-  ) %>% 
-  ggplot(aes(playTheBallAverageSpeed , color = oppositionTeam_is_souths)) +
-  geom_density() +
-  geom_vline(data=mu, aes(xintercept=total.mean, color= oppositionTeam_is_souths), linetype="dashed") +
-  facet_grid(year~., scales = "free") #+ 
-  #scale_color_manual(values=c(rainbow(2)))
-
-
-
-player_df %>% 
-  group_by(firstName, lastName) %>% 
-  summarise( total_tries = sum(tries, na.rm = TRUE)) %>% 
-  arrange(desc(total_tries))
 
 # Team score distribution
 match_table_df %>%
@@ -214,8 +136,7 @@ match_features_df %>%
 
 
 
-names(player_df)
-names(match_table_df)
+
 
 # Kick return metres vs hit up metres
 

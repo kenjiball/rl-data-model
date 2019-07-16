86b1973350c192f01f530f094bf0bccf0b6618d0
kenjiball <patrick.kenji.ball@gmail.com>
2019-07-16 22:06:41 +1000

    updates to call the working directory in function

--- data-preparation/data_api_request.R
+++ data-preparation/data_api_request.R
@@ -11,31 +11,38 @@ getwd()
 
 
 # Function to Read Player Data Json from API and save to file 
-grabData <- function(gameid,api_url,api_key,fileprefix){
+grabData <- function(gameid,api_url,api_key,fileprefix, wd_in){
   # Create API call from gameid
   API_str <- paste(api_url
                ,gameid
                ,api_key
                , sep = "", collapse = NULL)
+  # Set year from gameid
+  year <- substr(gameid,1,4)
   # Create fie name from gameid
-  file_name <- paste(fileprefix,gameid,".json", sep = "", collapse = NULL)
+  file_name <- paste(fileprefix,"_",gameid,".json", sep = "", collapse = NULL)
+  file_path <- paste0(wd_in, "/data/fox_",fileprefix,"/",year,"/")
   # Call API to get data for a game
   jsonData <- fromJSON(API_str)
   jsonDataPretty <- toJSON(jsonData, pretty = TRUE)
   # Write to file
-  write(jsonDataPretty, file_name)
+  write(jsonDataPretty, paste0(file_path,file_name))
 }
 
 
 # Single game 
-grabData(20180101,api_url,api_player_key,"playerdata_")
-grabData(20180101,api_url,api_match_key,"matchdata_")
-grabData(20180101,api_url,api_scoring_key,"scoringdata_")
+grabData(20170101,api_url,api_player_key,"playerdata",wd)
+grabData(20170101,api_url,api_match_key,"matchdata",wd)
+grabData(20170101,api_url,api_scoring_key,"scoringdata",wd)
 
+# Round of games
+sapply(20191401:20191408, grabData,api_url,api_player_key,"playerdata",wd)
+sapply(20191401:20191408, grabData,api_url,api_match_key,"matchdata",wd)
+sapply(20191401:20191408, grabData,api_url,api_scoring_key,"scoringdata",wd)
 
 # Vector of games
                  
-sapply(season_2019, grabData,api_url,api_player_key,"playerdata_")
-sapply(season_2019, grabData,api_url,api_match_key,"matchdata_")
-sapply(season_2019, grabData,api_url,api_scoring_key,"scoringdata_")
+sapply(season_2016, grabData,api_url,api_player_key,"playerdata",wd)
+sapply(season_2016, grabData,api_url,api_match_key,"matchdata",wd)
+sapply(season_2016, grabData,api_url,api_scoring_key,"scoringdata",wd)
 
